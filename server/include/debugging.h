1ccebc9585528ffcae4389bbaf2d692edab65030
yayamayaya <semenov.aleksandr@phystech.edu>
2024-12-20 02:45:19 +0300

    header для дебаггинга

---
+++ server/include/debugging.h
@@ -0,0 +1,27 @@
+#ifndef DEBUGGING_FUNCS
+#define DEBUGGING_FUNCS
+
+#define STARTING_USER_NUMBER 64
+
+#define LOG(...)
+
+#define _RETURN_ON_TRUE(statement, ret_val, ...)            \
+    do                                                      \
+    {                                                       \
+        if (statement)                                      \
+        {                                                   \
+            __VA_ARGS__                                     \
+            return ret_val;                                 \
+        }                                                   \
+    } while (0)
+
+
+enum SERVER_ERRORS
+{
+    USER_STACK_INIT_ERR     = 1321,
+    USER_DATA_BASE_INIT_ERR = 1322,
+    USER_DATABASE_REALC_ERR = 1323,
+};
+
+
+#endif
