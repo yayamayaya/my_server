ea9a9cae2e7048dfaad43ba1c296c15e40a96d6c
yayamayaya <semenov.aleksandr@phystech.edu>
2024-12-20 13:37:24 +0300

    новые ошибки

--- server/include/debugging.h
+++ server/include/debugging.h
@@ -5,6 +5,8 @@
 
 #define LOG(...)
 
+#define LOG_ERR(...)
+
 #define _RETURN_ON_TRUE(statement, ret_val, ...)            \
     do                                                      \
     {                                                       \
@@ -21,6 +23,7 @@ enum SERVER_ERRORS
     USER_STACK_INIT_ERR     = 1321,
     USER_DATA_BASE_INIT_ERR = 1322,
     USER_DATABASE_REALC_ERR = 1323,
+    STACK_ADD_ERR           = 1324,
 };
 
 
