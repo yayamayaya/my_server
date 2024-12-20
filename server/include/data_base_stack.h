094ec55328be8d71567398498e96c17b90c76c2f
yayamayaya <semenov.aleksandr@phystech.edu>
2024-12-21 01:26:39 +0300

    перенес структуры стека в хедер стека

--- server/include/data_base_stack.h
+++ server/include/data_base_stack.h
@@ -3,7 +3,33 @@
 
 #include "structs.h"
 
-#define DATA_BASE_LOCATION  "/user_data.txt"
+typedef struct user_data user_t;
+typedef struct user_base user_stack;
+typedef struct Ops st_op_table;
+
+typedef struct Ops
+{
+    ret_t (*add_user)(user_stack *self, user_t user_data);
+    no_ret_val_t (*rm_user)(user_stack *self, unsigned long int user_number);
+
+    unsigned long int (*find_user)(user_stack *self, const char *username);
+    user_t (*return_usr_data)(user_stack *self, unsigned long int user_number);
+    void (*dump)(user_stack *self);
+
+    ret_t (*stack_destructor)(user_stack *self);
+} st_op_table;
+
+typedef struct user_base
+{
+    user_t *users;
+
+    unsigned long int users_number;
+    unsigned long int stack_size;
+
+    st_op_table methods;
+} user_stack;
+
+#define DATA_BASE_LOCATION  "/user_data.db"
 
 ret_t init_user_stack(user_stack **stk_ptr);
 
