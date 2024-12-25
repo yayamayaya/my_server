58a5b96026b28ded0588d922125d69acd85b2906
yayamayaya <semenov.aleksandr@phystech.edu>
2024-12-25 03:13:37 +0300

    временные изменения в тестовом мейне

--- server/src/main.c
+++ server/src/main.c
@@ -4,16 +4,21 @@
 
 int main(int argc, char const *argv[])
 {
-    int ret_val = read_data_base();
-    _RETURN_ON_TRUE(ret_val, ret_val);
+    open_log();
+
+    ret_t ret_val = read_data_base();
+    _RETURN_ON_TRUE(ret_val, ret_val, close_log(););
 
     ret_val = register_new_user("sashanya11", "hui_tebe", "./new_rcv", "./new_snd");
-    _RETURN_ON_TRUE(ret_val, ret_val);
+    _RETURN_ON_TRUE(ret_val, ret_val, close_log(); finish_db_work(););
 
-    ret_val = check_pswd("user", "123");
-    LOG("> user password: %d\n", ret_val);
+    pswd_t pswd_check = check_pswd("user", "123");
+    LOG("> user password: %d\n", pswd_check);
 
     user_base_dump();
 
-    return 0;
+    ret_val = finish_db_work();
+
+    close_log();
+    return ret_val;
 }
