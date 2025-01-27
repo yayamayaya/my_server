d1ab449cf3e77af85d28799d301813324fd4e339
yayamayaya <semenov.aleksandr@phystech.edu>
2025-01-28 00:32:07 +0300

    перешёл с имитирования серверного интерфейса на локальном компьютере к созданию сервера на сокетах. Удалил все не нужные функции и файлы, начал работу над новыми функциями

--- server/src/main.c
+++ server/src/main.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-#include "data_base_work.h"
 #include "debugging.h"
+#include "server_start.h"
 
 int main(int argc, char const *argv[])
 {
     open_log();
 
-    ret_t ret_val = read_data_base();
-    _RETURN_ON_TRUE(ret_val, ret_val, close_log(););
-
-    ret_val = register_new_user("sashanya11", "hui_tebe", "./new_rcv", "./new_snd");
-    _RETURN_ON_TRUE(ret_val, ret_val, close_log(); finish_db_work(););
-
-    pswd_t pswd_check = check_pswd("user", "123");
-    LOG("> user password: %d\n", pswd_check);
-
-    user_base_dump();
-
-    ret_val = finish_db_work();
+    ret_t ret_val = run_server();
 
     close_log();
     return ret_val;
