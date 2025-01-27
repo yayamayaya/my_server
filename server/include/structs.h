c780f8960b281f8dd504df33314cce0dfe4196f5
yayamayaya <semenov.aleksandr@phystech.edu>
2025-01-28 00:34:55 +0300

    удалил лишние поля дескрипторов в структуре

--- server/include/structs.h
+++ server/include/structs.h
@@ -8,14 +8,12 @@ typedef char   pswd_t;
 typedef void   no_ret_val_t; 
 typedef int    ret_t;
 
+
 typedef struct user_data
 {
     unsigned long int user_number;
     char username[USERNAME_MAX_LENGTH];
     char password[USER_PSWRD_MAX_LEN];
-    int rcv_d;
-    int snd_d;
 } user_t;
-
 
 #endif
