d084deffa41e1d755c3102eca3885a024befa58f
yayamayaya <semenov.aleksandr@phystech.edu>
2025-01-31 22:41:30 +0300

    вновь изменил дизайн логов

--- server/src/data_base_stack.c
+++ server/src/data_base_stack.c
@@ -27,10 +27,10 @@ ret_t init_user_stack(user_stack **stk_ptr)
     assert(stk_ptr);
 
     user_stack *stack = (user_stack *)calloc(1, sizeof(user_stack));
-    _RETURN_ON_TRUE(!stack, USER_STACK_INIT_ERR, LOG("]> couldn't allocate memory for user stack\n"););
+    _RETURN_ON_TRUE(!stack, USER_STACK_INIT_ERR, LOG("}> couldn't allocate memory for user stack\n"););
 
     stack->users  = (user_t *)calloc(STARTING_USER_NUMBER, sizeof(user_t));
-    _RETURN_ON_TRUE(!stack->users, USER_DATA_BASE_INIT_ERR, LOG("]> couldn't allocate memory for data base\n"););
+    _RETURN_ON_TRUE(!stack->users, USER_DATA_BASE_INIT_ERR, LOG("}> couldn't allocate memory for data base\n"););
 
     stack->users_number   = 0;
     stack->stack_size     = STARTING_USER_NUMBER;
@@ -66,15 +66,15 @@ ret_t increase_stack_size(user_stack *self)
 {
     assert(self);
 
-    LOG("]> increasing user staack size\n");
+    LOG("}> increasing user staack size\n");
 
     self->stack_size <<= 2;
     user_t *base_holder = (user_t *)realloc(self->users, self->stack_size * sizeof(user_t));
-    _RETURN_ON_TRUE(!base_holder, USER_DATABASE_REALC_ERR, LOG("]> couldn't increase memory for data base\n"); self->stack_size >>= 2;);
+    _RETURN_ON_TRUE(!base_holder, USER_DATABASE_REALC_ERR, LOG("}> couldn't increase memory for data base\n"); self->stack_size >>= 2;);
 
     self->users = base_holder;
 
-    LOG("]> success\n");
+    LOG("}> success\n");
 
     return 0;
 }
@@ -84,7 +84,7 @@ unsigned long int find_user(user_stack *self, const char *username)
     assert(self);
     assert(username);
 
-    if (self->stack_size == 0) LOG("]> seeking trough empty stack\n");
+    if (self->stack_size == 0) LOG("}> seeking trough empty stack\n");
 
     for (long unsigned int usr_num = 0; usr_num < self->stack_size; usr_num++)
         if (!strcmp(self->users[usr_num].username, username))
@@ -99,7 +99,7 @@ user_t return_usr_data(user_stack *self, unsigned long int user_number)
 
     if (user_number > self->stack_size)
     {
-        LOG("]> seeking user number greater than stack size\n");
+        LOG("}> seeking user number greater than stack size\n");
         exit(NUMBER_GREATER_SIZE);
     }
 
@@ -131,7 +131,7 @@ no_ret_val_t rm_user(user_stack *self, unsigned long int user_number)
 
     if (user_number > self->stack_size)
     {
-        LOG("]> no such user number\n");
+        LOG("}> no such user number\n");
         return;
     }
 
@@ -148,21 +148,21 @@ void stack_dump(user_stack *self)
 {
     assert(self);
 
-    LOG("]> --------------------------------------------------------------------------\n");
-    LOG("]> stack dump: \n");
-    LOG("]> stack size: %lu\n", self->stack_size);
-    LOG("]> number of users: %lu\n", self->users_number);
+    LOG("}> --------------------------------------------------------------------------\n");
+    LOG("}> stack dump: \n");
+    LOG("}> stack size: %lu\n", self->stack_size);
+    LOG("}> number of users: %lu\n", self->users_number);
 
     for (unsigned long int i = 0; i < self->users_number; i++)
     {
-        LOG("]> -------------------------------------\n");
-        LOG("]> user number: %lu\n", i);
-        LOG("]> username: %s\n", self->users[i].username);
-        LOG("]> password: %s\n", self->users[i].password);
+        LOG("}> -------------------------------------\n");
+        LOG("}> user number: %lu\n", i);
+        LOG("}> username: %s\n", self->users[i].username);
+        LOG("}> password: %s\n", self->users[i].password);
     }
     
-    LOG("]> dump ended\n");
-    LOG("]> --------------------------------------------------------------------------\n");
+    LOG("}> dump ended\n");
+    LOG("}> --------------------------------------------------------------------------\n");
 
     return;
 }
