7f8b41ac5fcdb2f0c63a23eb5da588eb9406af0f
yayamayaya <semenov.aleksandr@phystech.edu>
2025-01-31 23:34:14 +0300

    убрал лишние тайпдефы из структур

--- server/include/structs.h
+++ server/include/structs.h
@@ -1,13 +1,12 @@
 #ifndef SERVER_STRUCTS
 #define SERVER_STRUCTS
 
+#include "debugging.h"
+
 #define USERNAME_MAX_LENGTH 32
 #define USER_PSWRD_MAX_LEN  16
 
 typedef char   pswd_t;
-typedef void   no_ret_val_t; 
-typedef int    ret_t;
-
 
 typedef struct user_data
 {
