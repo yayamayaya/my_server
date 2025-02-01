064796dff7fed853d5fc02e6c51e44e46f11aebc
yayamayaya <semenov.aleksandr@phystech.edu>
2025-02-01 13:03:52 +0300

    переименовал функции

--- server/src/server_start.c
+++ server/src/server_start.c
@@ -7,7 +7,7 @@
 #include "debugging.h"
 #include "server_start.h"
 #include "connection_work_func.h"
-#include "user_work_func.h"
+#include "request_check_func.h"
 
 ret_t run_server()
 {
@@ -19,5 +19,5 @@ ret_t run_server()
     _RETURN_ON_TRUE(work_proc_id == -1, -1, LOG_ERR(">> couldn't create new process:"));
     _RETURN_ON_TRUE(work_proc_id, connection_manage_process());
     
-    return user_work_process();
+    return request_check_process();
 }
