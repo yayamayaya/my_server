53306f04caa595670e30e31198b253fbf054d0c1
yayamayaya <semenov.aleksandr@phystech.edu>
2025-02-06 00:14:45 +0300

    временно убрал функции ipc

--- server/include/msg_ipc.h
+++ server/include/msg_ipc.h
@@ -15,6 +15,14 @@ struct msgbuf
     sockd_t descr;
 };  
 
+/*enum OPTIONS
+{
+    SND = 1,
+    RCV = 0
+};
+
 msqd_t init_ipc(const int ipc_number, const int flag);
+
+ret_t test_msg(msqd_t msgd, const int option, const int msg_type);*/
 
 #endif
