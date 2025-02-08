7d178a1cc7c15197b730d1eabe04b2003e17b348
yayamayaya <semenov.aleksandr@phystech.edu>
2025-02-08 23:44:33 +0300

    убрал всё лишнее, почистил кодстайл

--- server/include/request_check_func.h
+++ server/include/request_check_func.h
@@ -7,7 +7,7 @@
 #include "msg_ipc.h"
 #include "active_hosts_stack.h"
 
-#define HOST_UPDATE_TIME 1000
+#define HOST_UPDATE_TIME 5000
 #define HOST_RQST_TIME   5000
 
 ret_t request_check_process();
