05146c25d550a4c8d18cd8cfac1e3f605e185fd1
yayamayaya <semenov.aleksandr@phystech.edu>
2025-02-10 16:49:07 +0300

    сделал рефакторинг процесса реквестов

--- server/src/request_check_func.c
+++ server/src/request_check_func.c
@@ -14,7 +14,7 @@
 #include "sem_sync.h"
 
 
-ret_t create_all_process_data(process_data_st *p_data);
+ret_t create_all_process_objects(process_data_st *p_data);
 
 no_ret_val_t stacks_destr(process_data_st *p_data);
 
@@ -36,11 +36,12 @@ ret_t request_check_process()
 
     LOG("}> initialising all data struct:\n");
     process_data_st p_data = {};
-    ret_val = create_all_process_data(&p_data);
+    ret_val = create_all_process_objects(&p_data);
     _RETURN_ON_TRUE(ret_val, ret_val);
 
     LOG("}> starting poll cycle:\n");
-    poll_cycle(&p_data);
+    while (!check_kill_server_var())
+        poll_cycle(&p_data);
 
     LOG("}> server shutdown...\n");
     p_data.active_hosts->methods.dump(p_data.active_hosts);
@@ -49,7 +50,7 @@ ret_t request_check_process()
     return 0;
 }
 
-ret_t create_all_process_data(process_data_st *p_data)
+ret_t create_all_process_objects(process_data_st *p_data)
 {
     assert(p_data);
 
@@ -83,15 +84,15 @@ ret_t create_all_process_data(process_data_st *p_data)
         close(p_data->work_msg_s));
     LOG("}> done\n");
 
-    LOG("}> user stack initiation:\n");
+    /*LOG("}> user stack initiation:\n");
     user_stack *user_base = data_base_init();
     _RETURN_ON_TRUE(!user_base, ret_val,
         hosts->methods.stack_destructor(hosts);
         close(p_data->conn_msg_s);
         close(p_data->work_msg_s));
-    LOG("}> done\n");
+    LOG("}> done\n");*/
 
-    p_data->data_base       = user_base;
+    //p_data->data_base       = user_base;
     p_data->active_hosts    = hosts;
     
     return 0;
@@ -108,8 +109,8 @@ no_ret_val_t stacks_destr(process_data_st *p_data)
     unlink(CONN_WORK_UNIX_SOCKET_PATH);
     unlink(USER_WORK_UNIX_SOCKET_PATH);
 
-    finish_db_work(p_data->data_base);
-    p_data->active_hosts->methods.stack_destructor(p_data->active_hosts);
+    //finish_db_work(p_data->data_base);
+    //p_data->active_hosts->methods.stack_destructor(p_data->active_hosts);
 
     return;
 }
@@ -135,25 +136,22 @@ no_ret_val_t poll_cycle(process_data_st *p_data)
 {
     assert(p_data);
 
+    struct pollfd *hosts    = p_data->active_hosts->hosts;
+    unsigned long hosts_num = p_data->active_hosts->hosts_number;
+
     ret_t poll_ret_val = 0;
 
-    while (1)
+    if (update_active_hosts(p_data) == -1) return;
+
+    poll_ret_val = poll(hosts, hosts_num, HOST_RQST_TIME);
+    if (!poll_ret_val) return;
+    if (poll_ret_val == -1)
     {
-        if (check_kill_server_var()) break;
-        if (update_active_hosts(p_data) == 1) LOG("}> new host added\n");
-
-        poll_ret_val = 0;
-        poll_ret_val = poll(p_data->active_hosts->hosts, p_data->active_hosts->hosts_number, HOST_RQST_TIME);
-        if (poll_ret_val == -1)
-        {
-            LOG_ERR("}> poll error:");
-            continue;
-        }
-        
-        send_new_request(p_data, poll_ret_val);
-
-        if (0) break;
+        LOG_ERR("}> poll error:");
+        return;
     }
+    
+    send_new_request(p_data, poll_ret_val);
 
     return;
 }
@@ -162,51 +160,55 @@ ret_t update_active_hosts(process_data_st *p_data)
 {
     assert(p_data);
 
+    host_stack *hosts = p_data->active_hosts;
+
     static struct pollfd msg_descr = {0};
     msg_descr.fd        = p_data->conn_msg_s;
     msg_descr.events    = POLLIN;
 
     ret_t ret_val = poll(&msg_descr, 1, 0);
     _RETURN_ON_TRUE(ret_val == -1, 0, LOG_ERR("}> poll error:"));
-    if (!ret_val) return 0;
-    LOG("}> HOST REVENT: %d\n", msg_descr.revents);
+    _RETURN_ON_TRUE(ret_val ==  0, 0);
+    LOG("}> new host revent: %d\n", msg_descr.revents);
 
     int rcvd_descr = rcv_open_file_descriptor(p_data->conn_msg_s);
-    LOG("}> rcvd host descr == %d\n", rcvd_descr);
     _RETURN_ON_TRUE(rcvd_descr == -1, -1);
+    LOG("}> new host descr == %d\n", rcvd_descr);
 
     struct pollfd host_descr = {};
     host_descr.fd       = rcvd_descr;
     host_descr.events   = POLLIN;
-    _RETURN_ON_TRUE(p_data->active_hosts->methods.add_host(p_data->active_hosts, host_descr), HOST_ARR_REALLC_ERR);
+    _RETURN_ON_TRUE(hosts->methods.add_host(hosts, host_descr), HOST_ARR_REALLC_ERR);
+
+    LOG("}> new host added\n");
 
     return 1;
 }
 
-//создание новой нити исполнения
 no_ret_val_t send_new_request(process_data_st *p_data, ret_t poll_ret_val)
 {
-    if (!poll_ret_val) return;
+    int data_type       = 0;
+    sockd_t host_sock   = 0;
+    host_stack *hosts_s = p_data->active_hosts;
 
-    p_data->active_hosts->methods.dump(p_data->active_hosts);
+    hosts_s->methods.dump(p_data->active_hosts);
 
-    for (long unsigned int host_num = 0; host_num < p_data->active_hosts->hosts_number; host_num++)
-        if (p_data->active_hosts->hosts[host_num].revents & POLLIN)
+    for (long unsigned int host_num = 0; host_num < hosts_s->hosts_number; host_num++)
+        if (hosts_s->hosts[host_num].revents & POLLIN)
         {
             LOG("}> found requesting socket in host num: %d\n", host_num);
+            host_sock = p_data->active_hosts->hosts[host_num].fd;
 
-            sockd_t host_sock = p_data->active_hosts->hosts[host_num].fd;
-
-            int data_type = 0;
+            LOG("}> detecting receiving data type\n");
             ssize_t bytes_rcvd = recv(host_sock, &data_type, sizeof(int), 0);
-            LOG("}> bytes recieved: %d\n", bytes_rcvd);
             if (bytes_rcvd == 0)
             {
                 LOG("}> the connection have been closed, deleting host descr: %d\n", host_sock);
                 close(host_sock);
-                p_data->active_hosts->methods.rm_host(p_data->active_hosts, host_sock);
+                hosts_s->methods.rm_host(p_data->active_hosts, host_sock);
                 continue;
             }
+            LOG("}> bytes recieved: %d\n", bytes_rcvd);
             LOG("}> request data type: %d\n", data_type);
 
             if (send_open_file_descriptor(p_data->work_msg_s, p_data->active_hosts->hosts[host_num].fd) == -1)
